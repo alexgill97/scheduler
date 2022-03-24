c24e28d9517b32530e76e303382c696b763e194b
Alex <alex.gillespie@live.com>
2022-03-24 14:29:05 -0400

    Began Appointment functionallity

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -18,7 +18,7 @@ function DayListItem(props) {
     'day-list__item--full': !props.spots,
   });
   return (
-    <li className={liClass} onClick={() => props.setDay(props.name)}>
+    <li className={liClass} onClick={props.setDay}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,19 @@
+import classNames from 'classnames';
 import React from 'react';
+import 'components/InterviewerListItem.scss';
 
 function InterviewerListItem(props) {
+  let liClass = classNames('interviewers__item', {
+    'interviewers__item--selected': props.selected,
+  });
   return (
-    <li className="interviewers__item">
+    <li onClick={props.setInterviewer} className={liClass}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && <p>{props.name}</p>}
     </li>
   );
 }
